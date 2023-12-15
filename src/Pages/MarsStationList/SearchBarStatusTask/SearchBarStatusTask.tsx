5acab0cd4a9645297b40a5db04ebc6e7031d51ce
RedAlexDad <boss6852@gmail.com>
2023-12-15 09:37:48 +0300

    Переделан поиск по статусам

--- src/Pages/MarsStationList/SearchBarStatusTask/SearchBarStatusTask.tsx
+++ src/Pages/MarsStationList/SearchBarStatusTask/SearchBarStatusTask.tsx
@@ -1,61 +1,98 @@
-import "./SearchBarStatusTask.sass"
-import {useDispatch} from "react-redux";
-import {updateStatusTask} from "../../../store/Search";
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Dispatch, SetStateAction} from "react";
-import { useAuth } from "../../../hooks/useAuth";
+import * as React from 'react';
+import { Theme, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+import { useDispatch } from 'react-redux';
+import { updateStatusTask } from '../../../store/Search';
+import { Dispatch, SetStateAction } from 'react';
+import { useAuth } from '../../../hooks/useAuth';
 
-const SearchBarStatusTask = ({status_task, setUpdateTriggerParent}: {
-    status_task: string[];
-    setUpdateTriggerParent: Dispatch<SetStateAction<boolean>>;
-}) => {
-    const {is_moderator} = useAuth()
-    const dispatch = useDispatch();
-
-    const handleStatusSelect = (status: string) => {
-        const updatedStatus = [...status_task];
-        if (updatedStatus.includes(status)) {
-            // Удаляем статус, если он уже выбран
-            const index = updatedStatus.indexOf(status);
-            updatedStatus.splice(index, 1);
-        } else {
-            // Добавляем статус, если он еще не выбран
-            updatedStatus.push(status);
-        }
-        dispatch(updateStatusTask(updatedStatus));
-        setUpdateTriggerParent(true);
-    };
-
-    return (
-        <div className="dropdown-wrapper">
-            <DropdownButton id="dropdown-item-button" title="Статус задачи" className="dropdown-button">
-                <Dropdown.ItemText className="dropdown-item-text">Выберите статус(ы)</Dropdown.ItemText>
-                {!is_moderator &&
-                    <Dropdown.Item as="button" active={status_task.includes('1')}
-                                onClick={() => handleStatusSelect('1')} className={`dropdown-item action`}>
-                        Черновик
-                    </Dropdown.Item>
-                }
-                <Dropdown.Item as="button" active={status_task.includes('2')}
-                               onClick={() => handleStatusSelect('2')}
-                               className={`dropdown-item another-action`}>
-                    В работе
-                </Dropdown.Item>
-                <Dropdown.Item as="button" active={status_task.includes('3')}
-                               onClick={() => handleStatusSelect('3')}
-                               className={`dropdown-item something-else`}>
-                    Завершена
-                </Dropdown.Item>
-                <Dropdown.Item as="button" active={status_task.includes('4')}
-                               onClick={() => handleStatusSelect('4')}
-                               className={`dropdown-item something-else`}>
-                    Отменена
-                </Dropdown.Item>
-            </DropdownButton>
-        </div>
-    );
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+      color: 'white', // Цвет текста в выпадающем списке
+    },
+  },
 };
 
-export default SearchBarStatusTask;
+const statusOptions = [
+  { value: '1', label: 'Черновик' },
+  { value: '2', label: 'В работе' },
+  { value: '3', label: 'Завершена' },
+  { value: '4', label: 'Отменена' },
+];
+
+export default function SearchBarStatusTask({
+  status_task,
+  setUpdateTriggerParent,
+}: {
+  status_task: string[];
+  setUpdateTriggerParent: Dispatch<SetStateAction<boolean>>;
+}) {
+  const { is_moderator } = useAuth();
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  const handleChange = (event: SelectChangeEvent<typeof status_task>) => {
+    const {
+      target: { value },
+    } = event;
+
+    const updatedStatus = Array.isArray(value) ? value : [value];
+
+    dispatch(updateStatusTask(updatedStatus));
+    setUpdateTriggerParent(true);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel 
+        id="demo-multiple-chip-label" 
+        sx={{ color: 'white'}} // Цвет текста над выпадающим списком
+        >
+            Статус задачи
+        </InputLabel>
+        <Select
+          labelId="demo-multiple-chip-label"
+          id="demo-multiple-chip"
+          multiple
+          value={status_task}
+          onChange={handleChange}
+          input={<OutlinedInput id="select-multiple-chip" label="Статус задачи" />}
+          renderValue={(selected) => (
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5, color: 'white'}}>
+              {selected.map((value) => (
+                <Chip key={value} label={statusOptions.find((opt) => opt.value === value)?.label} />
+              ))}
+            </Box>
+          )}
+          MenuProps={MenuProps}
+          sx={{ '& .MuiChip-root': { color: 'white' } }} // Цвет текста в выбранных элементах
+        >
+          {statusOptions.map((option) => (
+            <MenuItem
+              key={option.value}
+              value={option.value}
+              style={{ 
+                fontWeight: status_task.includes(option.value) ? theme.typography.fontWeightMedium : theme.typography.fontWeightRegular, 
+                color: 'black', // Цвет текста в выпадающем списке
+            }}
+            >
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
